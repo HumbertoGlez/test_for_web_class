c2da2d82b1436ad90b143229fecaa970e0978b1f
Humberto González Sánchez <hglez.98s@gmail.com>
2020-09-10 20:50:10 -0500

    Finished dynamic calculator assignment

--- DynamicCalculator/utils.js
+++ DynamicCalculator/utils.js
@@ -1,5 +1,19 @@
 function get_element_li (name, price) {
-  return `<li class="added-item">name: ${name} price: ${price}  <button id="remove-item">remove</button></li>`
+  let newLi = document.createElement("li");
+  newLi.classList.add("added-item");
+  if (isNaN(price)) {
+    newLi.classList.add("invalid-input");
+  }
+  newLi.appendChild(document.createTextNode('name: ' + name + '\tprice: '));
+  let priceSpan = document.createElement("span");
+  priceSpan.appendChild(document.createTextNode(price));
+  priceSpan.classList.add("price-value");
+  newLi.appendChild(priceSpan);
+  let rmvButton = document.createElement("button");
+  rmvButton.classList.add("remove-item");
+  rmvButton.appendChild(document.createTextNode('remove'));
+  newLi.appendChild(rmvButton);
+  return newLi;
 }
 
 let add_item_to_list_with_template = (template_function) => {
@@ -11,8 +25,19 @@ let add_item_to_list_with_template = (template_function) => {
     */
     let itemName = document.querySelector("#item-name").value;
     let itemPrice = document.querySelector('#item-value').value;
+    if (itemName == '' || itemPrice.trim() == '') return;
     let template = template_function(itemName, itemPrice);
-    document.getElementById("items").innerHTML += template;
+    let itemList = document.getElementById("items");
+    itemList.appendChild(template);
+    itemList.lastElementChild.querySelector(".remove-item").addEventListener("click", (event) => 
+      remove_item(event.target.parentNode)  
+    );
+    if (!isNaN(itemPrice)) {
+      let total = Number(document.getElementById("total").innerText);
+      console.log(total);
+      total += Number(itemPrice);
+      document.getElementById("total").innerText = total;
+    }
   }
 }
 let addHandler = add_item_to_list_with_template(get_element_li);
@@ -26,6 +51,13 @@ let addHandler = add_item_to_list_with_template(get_element_li);
 */
 
 let remove_item  = (node_to_remove) => {
-    let element_to_delete = document.querySelector("selector").lastElementChild;
-    element_to_delete.parentNode.removeChild(element_to_delete);
+    let itemList = document.getElementById("items");
+    let priceToSubstract = node_to_remove.querySelector('.price-value').innerText;
+    if (!isNaN(priceToSubstract)) {
+      let total = Number(document.getElementById("total").innerText);
+      console.log(total);
+      total -= Number(priceToSubstract);
+      document.getElementById("total").innerText = total;
+    }
+    itemList.removeChild(node_to_remove);
 }
